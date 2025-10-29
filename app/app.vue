3d2ebcbc5de9308b200a86e8e6030c5fb5176d3d
Joan Mejia <joanmejiaa@gmail.com>
2025-10-29 12:14:33 -0500

    Commit Inicial

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <Home/>
+</template>
+
+<script lang="ts" setup>
+import Home from '~/components/home.vue'
+
+</script>
+
+<style>
+
+</style>
---
+++ app/components/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Bienvenido a miUltralink</h1>
+    <UApp>
+        <UButton icon="i-lucide-rocket" size="md" color="primary" variant="solid">Button</UButton>
+    </UApp>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
